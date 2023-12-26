8477958f01ec48ff70d72385a31d44a5c89f6cba
Jeff Lai <jefflai0412@gmail.com>
2023-12-27 01:31:44 +0800

    finish detecting

--- Image_processing/arucoMarkGenerator.py
+++ Image_processing/arucoMarkGenerator.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
 
+markerSize = 7
+markerNumber = 50
 
 def generate(number):
-    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
+    dictionary = cv2.aruco.getPredefinedDictionary(getattr(cv2.aruco, f"DICT_{markerSize}X{markerSize}_{markerNumber}"))
     for id in range(number):
         image = np.zeros((300, 300, 1), dtype="uint8")
         image = cv2.aruco.generateImageMarker(dictionary, id, 300, image, 1)
-        cv2.imwrite("arucoMarkers/{}__4X4_50.png".format(id), image)
+        cv2.imwrite(f"arucoMarkers/{id}__{markerSize}X{markerSize}_{markerNumber}.png", image)
 
 
 generate(30)
